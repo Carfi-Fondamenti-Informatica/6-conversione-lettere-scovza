a9f287ec408734eeaefeed463860d342124595b3
Tommaso Scorza <91656025+scovza@users.noreply.github.com>
2021-11-30 11:28:47 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,18 @@
+using namespace std;
 
+bool confronto (char a){
+
+    int num = (int) a;
+
+    if (num >= 65 && num <=90){
+
+        return true;
+    }else if (num >= 97 && num <= 122){
+
+        return true;
+    }else {
+
+        return false;
+    }
+
+}
