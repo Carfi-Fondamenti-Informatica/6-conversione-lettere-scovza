31a11a2fef585019c514b3fcbf505bff8be1a241
Tommaso Scorza <91656025+scovza@users.noreply.github.com>
2021-11-30 11:30:59 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,35 @@
 #include <iostream>
+#include "lib.h"
 
-int main(){
-  return 0;
+using namespace std;
+
+int main() {
+
+    char a;
+
+    cin >> a;
+
+    if (confronto (a) == true) {
+
+        int num = (int) a;
+
+        if (num >= 65 && num <=90){
+
+            char end = (char) num+32;
+
+            cout << end;
+
+        }else if(num >= 97 && num <=122){
+
+            char end = (char) num-32;
+
+            cout << end;
+        }
+
+    }else {
+
+        cout << "errore" << endl;
+    }
+    
+    return 0;
 }
